a2f9f0651319f9b6b42dffa098cee3ad470ed0dd
Marvin <67420091+TixDK@users.noreply.github.com>
2023-04-11 21:45:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 #Data reader, UI
+
+
+
+#Data collector 
+
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.ok.dk/privat/produkter/el/priser'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+def price():
+    try:
+        price = soup.find('div', class_='flex-table__cell cell--val hidden-xs')
+        
+        if price is not None:
+            data_price = price.text.strip()
+            print(f"Prisen er {data_price}")
+        else:
+            print("Price isn't found")
+    except Exception as error:
+        print(f"Fejl: {error}")
+
+
+price()
+
+
+
+
+
+
