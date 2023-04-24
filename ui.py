eddf0d4bf71b767a6cf53b32391d43c7c1a31ec8
Marvin <67420091+TixDK@users.noreply.github.com>
2023-04-24 18:40:05 +0200

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,36 +1,80 @@
+import time
+import tkinter
+import customtkinter
 import main
-from tkinter import *
-import customtkinter as frame
-import db
+import os
+import database
 
 
-def UI():
-    pris = main.price()
-
-    frame.set_appearance_mode("Dark")
-    frame.set_default_color_theme("blue")
-
-    app = frame.CTk()
-    app.geometry('750x500')
-    app.resizable(0,0)
-    app.title("Energi Måler")
 
 
-    nuværende_el_pris = frame.CTkLabel(app, text=f'Nuværende Kw/t pris: {pris} Kr')
-    nuværende_el_pris.configure(font=('Microsoft YaHei UI Light', 16))
-    nuværende_el_pris.place(x=10, y= 10)
+def Data_Command():
+    Data = database.DataCollector()
+    Data.create_database()
+    create_database.destroy()
+
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.geometry('750x500')
+app.resizable(0,0)
+app.title("Energi Monitor")
+
+frame = customtkinter.CTkFrame(master=app)
+frame.pack(pady=20, padx=40, fill="both", expand=True)
+
+create_database = customtkinter.CTkButton(master=frame, text="Create DataBase", command=Data_Command)
+create_database.place(relx=0.75, rely=0.9)
 
 
-    creation_data = db.DataInsertion()
+power = main.power_collector()
+total = main.total_collector()
+
+
+
+
+def ui():
     
-
-    data_knap = frame.CTkButton(master=app, text="Create Database", command=creation_data.create_data())
-    data_knap.place(relx=0.8, rely= 0.92)
-
-    data_knap = frame.CTkCheckBox(master=app, text="Update Data")
-    data_knap.place(relx=0.8, rely= 0.85)     
-
-
+    
+    
+    title = customtkinter.CTkLabel(master=frame, text="Energi Monitor")
+    title.configure(font=('Microsoft YaHei UI Light', 24))
+    title.place(relx=0.38, rely=0.01)
+    
+    
+    
+    price_label = customtkinter.CTkLabel(master=frame, text=f"Kw/t pris: {main.price_getter()} kr")
+    price_label.configure(font=('Microsoft YaHei UI Light', 24))
+    price_label.place(relx=0.02, rely=0.4)
+    
+    
+    power_label = customtkinter.CTkLabel(master=frame, text=f"Nuværende forbrug: {power}")
+    power_label.configure(font=('Microsoft YaHei UI Light', 16))
+    power_label.place(relx=0.02, rely=0.6)
+    
+    
+    total_label = customtkinter.CTkLabel(master=frame, text=f"Totalt forbrug: {total}")
+    total_label.configure(font=('Microsoft YaHei UI Light', 16))
+    total_label.place(relx=0.02, rely=0.65)
+    
+    price_label = customtkinter.CTkLabel(master=frame, text=f"Pris: {total} kr")
+    price_label.configure(font=('Microsoft YaHei UI Light', 16))
+    price_label.place(relx=0.02, rely=0.72)
+    
+    
+    energy_box = customtkinter.CTkCheckBox(master=frame, text="Update Data")
+    energy_box.place(relx=0.025, rely=0.91)
+    
+    
+    try:
+        if os.path.exists('data.db'):
+            create_database.destroy()
+    except Exception as err:
+        print(f"Error: {err}")
+    
     
     app.mainloop()
-
+    
+    
+    
